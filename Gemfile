0ef0ec310fd45fad47b7690ba345a98f5ab5dd4c
Ram <ram23452001@yahoo.com>
2011-05-06 15:53:31 -0700

    Added site layout

--- Gemfile
+++ Gemfile
@@ -5,10 +5,11 @@ gem 'rails', '3.0.3'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
  group :development do
-   gem 'rspec-rails', '2.0.0.beta.18'
+   gem 'rspec-rails', '2.5.0'
  end
 
  group :test do
-   gem 'rspec', '2.0.0.beta.18'
+   gem 'rspec', '2.5.0'
+   gem 'webrat'
  end
  
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,23 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", 
+              :content => "Ruby on Rails Tutorial Sample app | Home")
+    end
+    
+    it "should have non blank body" do
+      get "home"
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,6 +27,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+          get 'contact'
+          response.should have_selector("title", 
+                  :content => "Ruby on Rails Tutorial Sample app | Contact")
+        end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+          get 'about'
+          response.should have_selector("title", 
+                  :content => "Ruby on Rails Tutorial Sample app | About")
+    end
+  end
 end
