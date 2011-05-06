468f9a47b9a1e70e6f42eade8c952519c604e635
Ram <ram23452001@yahoo.com>
2011-05-06 16:12:15 -0700

    Added site layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returrn a title on page
+  def title 
+      base_title = "Ruby on Rails Tutorial Sample app"    
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end    
+  end
 end
